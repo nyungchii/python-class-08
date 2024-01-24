39e644ffab6d912fc15db6cf76c3649935831e6a
nyungchii <139519900+nyungchii@users.noreply.github.com>
2024-01-24 15:53:31 +0700

    Add files via upload

---
+++ 510_23.py
@@ -0,0 +1,21 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num = 1
+... while num <= 21:
+...     print(num, end=",")
+...     num += 2
+... num = 2
+... while num <= 17:
+...     print(num, end=",")
+...     num += 3
+... num = 100
+... while num >= -90:
+...     print(num, end=",")
+...     num -= 10
+... num = -30
+... while num <= 30:
+...     print(num, end=",")
+...     num += 10
+... num = 15
+... while num <= 55:
+...     print(num, end=",")
