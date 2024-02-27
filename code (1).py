3d6bfdeafe4bdf01cbcfee8abc03aab840a4ed94
nyungchii <139519900+nyungchii@users.noreply.github.com>
2024-02-27 14:15:57 +0700

    Add files via upload

---
+++ code (1).py
@@ -0,0 +1,29 @@
+# 1
+n = 1
+while n <= 21:
+    print(n, end=', ')
+    n += 2
+print()
+# 2
+n = 2
+while n <= 17:
+    print(n, end=', ')
+    n += 3
+print()
+# 3
+n = 100
+while n >= -100:
+    print(n, end=', ')
+    n -= 10
+print()
+# 4. -30,-20-10,0,10,20,30
+n = -30
+while n <= 30:
+    print(n, end=', ')
+    n += 10
+print()
+# 5
+n = 15
+while n <= 55:
+    print(n, end=', ')
+    n += 8
